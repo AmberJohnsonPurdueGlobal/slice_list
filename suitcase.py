c6c2f011a932f99d88109a7a5a4c82a8a927d371
AmberJohnsonPurdueGlobal <107158761+AmberJohnsonPurdueGlobal@users.noreply.github.com>
2023-01-25 09:42:35 -0800

    Create suitcase.py

---
+++ suitcase.py
@@ -0,0 +1,12 @@
+#create a list of items
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+#modify the list to display the first two items
+beginning = suitcase[0:2]
+#Display items
+print(beginning)
+#Slice the list to display the middle two items
+sliced_list = suitcase[2:4]
+#Create a new list from the sliced list
+middle = sliced_list
+#Display the new list called middle
+print(middle)
